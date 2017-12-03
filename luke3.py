5feb75fb4ad2331719b7675c22d510f21a840390
Christine Winge <christine.winge@gmailcom>
2017-12-03 16:38:06 +0100

    finished luke3

---
+++ luke3.py
@@ -0,0 +1,67 @@
+from PIL import Image
+
+   
+def getRed(im, x, y):
+    red, g, b = im.getpixel((x, y))
+    return red
+
+def getAllReds(im, width, height):
+    reds = []
+    for y in range(height):
+        for x in range(width):
+            reds.append(getRed(im, x, y))
+    return reds
+
+def sjekkPartall(tall):
+    return tall%2 == 0
+
+
+def decodeByte(byte):
+    number = 0
+    for i in range(8):
+        if byte[i]:
+            number += 2**i
+    return number
+
+def listToByteArray(lst):
+    byter = bytearray(b'\x00\x0f')
+    for i in range(0, len(lst), 8):
+        byte = decodeByte(lst[i:i+8])
+        byter.append(byte)
+    return byter
+    
+
+def makeLSBList(lst):
+    LSBList = []
+    for elem in lst:
+        if sjekkPartall(elem):
+            LSBList.append(False)
+        else:
+            LSBList.append(True)
+    return LSBList   
+
+rgbImage = Image.open("bilde.png")
+
+
+width, height = rgbImage.size
+
+reds = getAllReds(rgbImage, width, height)
+
+
+print(reds[0:5])
+bitList = makeLSBList(reds)
+
+byteArray = listToByteArray(bitList)
+
+
+resultat = byteArray[0:126].decode('utf-8')
+
+
+
+print(resultat)
+
+print(len(bitList)/1280)
+
+byte = [True, True, True, True, True, True, True, True]
+
+print(decodeByte(byte))
