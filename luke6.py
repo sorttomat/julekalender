26fc69cfc28237b01956019cf6fa0ebf6c9879d4
Christine Winge <christine.winge@gmailcom>
2017-12-06 23:40:29 +0100

    Updated luke6

--- luke6.py
+++ luke6.py
@@ -1,33 +1,54 @@
-newFile = open("HovedstadLatLong.txt", "w")
+from math import radians, cos, sin, asin, sqrt
+# Distance between two lat/lng coordinates in km using the Haversine formula
+def getDistanceFromLatLng(latOslo, lngOslo, latDest, lngDest): # use decimal degrees
+    r=6371 # radius of the earth in km
+    latOslo=radians(latOslo)
+    latDest=radians(latDest)
+    lat_dif=latDest-latOslo
+    lng_dif=radians(lngDest-lngOslo)
+    a=sin(lat_dif/2.0)**2+cos(latOslo)*cos(latDest)*sin(lng_dif/2.0)**2
+    d=2*r*asin(sqrt(a))
+    return d # return km
+
+def hentAvstander(filename, latitudeOslo, longitudeOslo):
+    avstander = []
+    with open(filename) as infile:
+        heading = infile.readline()
+        lines = infile.readlines()
+        geokoder = []
+        for line in lines:
+            if "\tHovedstad" in line:
+                line = line.lower()
+                line = line.split("\t")
+                if line[4] not in geokoder:
+                    latitude = float(line[12])
+                    longitude = float(line[13])
+                    avstander.append(getDistanceFromLatLng(latitudeOslo, longitudeOslo, latitude, longitude))
+                    geokoder.append(line[4])
+    infile.close()
+    return sorted(avstander)
 
 
-with open("verda.txt") as infile:
-    heading = infile.readline()
-    lines = infile.readlines()
-    for line in lines:
-        if "\tHovedstad" in line:
-            line = line.lower()
-            line = line.split()
-            g = 0
-            la = 10
-            lo = la + 1
-            geoname = line[g]
-            while geoname[0].isdigit() == False:
-                g += 1
-                geoname = line[g]
+latitudeOslo = 59.911491
+longitudeOslo = 10.757933
+fart = 7274 #km/h
+totalDrivstoff = fart * 24 #timer
 
-            latitude = line[la]
-            longitude = line[lo]
-            while "." not in latitude or latitude[0].isalpha():        
-                la += 1
-                latitude = line[la]
-                longitude = line[la + 1]
+avstander = hentAvstander("verda.txt", latitudeOslo, longitudeOslo)
 
-            skriv = geoname + " " + latitude + " " + longitude + "\n"
-            newFile.write(skriv)        
 
-newFile.close()
-infile.close()
+nesteDest = avstander[0]* 2
+antallReiser = 0
+teller = 0
+
+while totalDrivstoff - nesteDest > 0:
+    antallReiser += 1
+    totalDrivstoff -= nesteDest
+    teller += 1
+    nesteDest = avstander[teller] * 2
+
+print(antallReiser)
+
 
 
 
--- nissefar.py
+++ nissefar.py
@@ -1,14 +1,4 @@
-from math import radians, cos, sin, asin, sqrt
-# Distance between two lat/lng coordinates in km using the Haversine formula
-def getDistanceFromLatLng(latOslo, lngOslo, latDest, lngDest): # use decimal degrees
-    r=6371 # radius of the earth in km
-    latOslo=radians(latOslo)
-    latDest=radians(latDest)
-    lat_dif=latDest-latOslo
-    lng_dif=radians(lngDest-lngOslo)
-    a=sin(lat_dif/2.0)**2+cos(latOslo)*cos(latDest)*sin(lng_dif/2.0)**2
-    d=2*r*asin(sqrt(a))
-    return d # return km
+
 
 dic = {}
 with open("HovedstadLatLong.txt", "r") as infile:
@@ -19,8 +9,7 @@ with open("HovedstadLatLong.txt", "r") as infile:
 
 infile.close()
 
-latitudeOslo = 59.911491
-longOslo = 10.757933
+
 
 
 
@@ -38,18 +27,3 @@ for key in dic:
     avstander.append(dic[key][2])
 
 avstander = sorted(avstander)
-
-fart = 7274 #km/h
-totalDrivstoff = fart * 24 #timer
-nesteDest = avstander[0]* 2
-antallReiser = 0
-teller = 0
-
-while totalDrivstoff - nesteDest > 0:
-    antallReiser += 1
-    totalDrivstoff -= nesteDest
-    teller += 1
-    nesteDest = avstander[teller] * 2
-
-
-print(antallReiser)
